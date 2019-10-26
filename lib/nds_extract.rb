3061aa8e9f16770a9ae594c8e7144b728d48c41b
Nethelbert <gilblasse@gmail.com>
2019-10-26 18:13:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,11 +12,9 @@ def directors_totals(nds)
   row_index = 0
   
   while row_index < nds.count do
-    director_name = nds[row_index][:name]
-    director_movies = nds[row_index][:movies]
-    director_movies_gross = gross_for_director(director_movies)
+    director_hash = nds[row_index]
+    director_movies_gross = gross_for_director(director_hash)
     
-    result[director_name] = director_movies_gross
     row_index += 1 
   end
 
@@ -31,13 +29,5 @@ end
 
 def gross_for_director(director_data)
   total = 0
-  movie_row_index = 0
-  
-  
-  while movie_row_index < director_data.count do
-    total += director_data[movie_row_index][:worldwide_gross]
-    movie_row_index += 1
-  end
-  
-  total
+  director_data.class
 end
