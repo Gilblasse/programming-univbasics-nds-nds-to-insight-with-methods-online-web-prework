b9e02bdc962c3ae4e490e90abe1388afd76ba3c7
Nethelbert <gilblasse@gmail.com>
2019-10-26 17:51:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,9 +33,9 @@ def gross_for_director(director_data)
   total = 0
   movie_row_index = 0
   
-  binding.pry
   
   while movie_row_index < director_data.count do
+    binding.pry
     total += director_data[movie_row_index][:worldwide_gross]
     movie_row_index += 1
   end
